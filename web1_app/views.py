baf7be2cc58457d8f86702787307a2025721396e
Anna <afgnkad@gmail.com>
2025-02-21 14:55:05 +0300

    update db on page

--- web1_app/views.py
+++ web1_app/views.py
@@ -4,6 +4,16 @@ from .models import Data
 # Create your views here.
 
 def indexpage(request):
-    data = Data.objects.using('data').all()#Data.objects.all()
-    #print(data[1])
+    data = Data.objects.using('data').all()
+    if request.method == "POST":
+        data_post = request.POST
+        #user = data.get("user") request.POST.get('selected_id') 
+        id = data_post.get("select")
+        number = data_post.get("number")
+        name = data_post.get("name")
+        post_data = [id, number, name]
+        upd_profile = Data.objects.using('data').get(id=id)
+        upd_profile.name, upd_profile.number = name, number
+        upd_profile.save(using='data')
+        return render(request, 'index.html', {'data': data, 'post_data': post_data})
     return render(request, 'index.html', {'data': data})
