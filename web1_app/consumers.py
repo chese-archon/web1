028bb7f54525cbd19d315995d9bf374ec11e5385
Anna <afgnkad@gmail.com>
2025-03-17 15:31:07 +0300

    websocket short notify

--- web1_app/consumers.py
+++ web1_app/consumers.py
@@ -1,22 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer, WebsocketConsumer
 
-
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
-
-    def disconnect(self, close_code):
-        pass
-
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-
-        self.send(text_data=json.dumps({"message": message}))
-
-##
-
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.channel_layer.group_add("notifications_group", self.channel_name)
@@ -27,5 +11,6 @@ class NotificationConsumer(AsyncWebsocketConsumer):
 
     async def send_notification(self, event):
         message = event["message"]
+        print('from websocket:', message)
         await self.send(text_data=json.dumps({"message": message}))
 
--- web1_app/routing.py
+++ web1_app/routing.py
@@ -1,14 +1,14 @@
 # Пример routing.py для приложения
 from django.urls import re_path
-from .consumers import ChatConsumer, NotificationConsumer
+from .consumers import NotificationConsumer #ChatConsumer
 #
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/$', ChatConsumer.as_asgi()),
+    #re_path(r'ws/chat/$', ChatConsumer.as_asgi()),
 
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),#
+    #re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),#
     re_path(r'ws/chat/notifications/$', NotificationConsumer.as_asgi()),#
 ]
 #
